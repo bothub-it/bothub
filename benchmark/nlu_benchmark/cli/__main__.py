08d948b9c17188142a84e82669a717f85a7facc8
Douglas Paz <eu@douglaspaz.com>
2018-08-03 16:55:19 -0300

    Move version 1, add logger

--- benchmark/nlu_benchmark/cli/__main__.py
+++ benchmark/nlu_benchmark/cli/__main__.py
@@ -1,9 +1,17 @@
 import sys
+import logging
 import plac
 
+from . import logger as cli_logger
+from ..services import logger as services_logger
 from .tools.bothub import get_token as bothub_get_token
 from .run import run
 
+
+LOGGER_FORMAT = {
+    'StreamHandler': '%(name)-22s - %(levelname)-8s - %(message)s',
+    'FileHandler': '%(asctime)-15s - %(name)-22s - %(levelname)-8s - %(message)s'
+}
 
 commands = {
     'bothub_get_token': bothub_get_token,
@@ -11,8 +19,27 @@ commands = {
 }
 
 @plac.annotations(
+    debug=plac.Annotation(kind='flag', abbrev='D'),
     command=plac.Annotation(choices=commands.keys()))
-def main(command, *args):
-    plac.call(commands.get(command), args)
+def main(debug, command, *args):
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG if debug else logging.INFO)
+    ch.setFormatter(logging.Formatter(LOGGER_FORMAT.get('StreamHandler')))
+    fh = logging.FileHandler('nlu_benchmark.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter(LOGGER_FORMAT.get('FileHandler')))
+
+    for logger in [cli_logger, services_logger]:
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+
+    cli_logger.info(f'NLU Benchmark running {command} command')
+
+    try:
+        plac.call(commands.get(command), args)
+    except Exception as e:
+        cli_logger.exception(e)
+
 
 plac.call(main, sys.argv[1:])
--- benchmark/nlu_benchmark/cli/run.py
+++ benchmark/nlu_benchmark/cli/run.py
@@ -1,36 +1,63 @@
-import os
 import plac
 import yaml
+import json
 import requests
 
+from . import logger
 from ..services.bothub import Bothub
 
 
 @plac.annotations(
     data_file_path=plac.Annotation(),
+    data_file_type=plac.Annotation(kind='option', choices=['yaml', 'json']),
     bothub_user_token=plac.Annotation(kind='option'))
-def run(data_file_path, bothub_user_token=None):
-    if not os.path.isfile(data_file_path):
-        return print(f'{data_file_path} not exists')
+def run(data_file_path, data_file_type='yaml', bothub_user_token=None):
+    try:
+        data_file = open(data_file_path, 'r')
+    except Exception as e:
+        logger.error(f'Can\'t open the data file "{data_file_path}"')
+        raise e
 
-    data_file = open(data_file_path, 'r')
-    data = yaml.load(data_file)
+    try:
+        if data_file_type == 'yaml':
+            data = yaml.load(data_file)
+        elif data_file_type == 'json':
+            data = json.load(data_file)
+    except Exception as e:
+        raise e
+
+    logger.debug(f'data loaded to run benchmark {data}')
 
     bothub_global_config = Bothub.get_current_global_config()
 
     bothub_user_token = bothub_user_token or bothub_global_config.get('user_token')
     assert bothub_user_token
+    logger.debug(f'using {bothub_user_token} bothub user token')
 
+    logger.info('Starting Bothub benchmark...')
     bothub = Bothub(user_token=bothub_user_token)
+
     create_repository_response = bothub.create_temporary_repository(
         data.get('language'))
+
     try:
         create_repository_response.raise_for_status()
     except requests.exceptions.HTTPError as e:
-        return print('Temporary repository not created :(\n' +
-                     str(create_repository_response.content))
+        logger.error('Bothub temporary repository not created')
+        logger.debug(create_repository_response.text)
+        raise e
+
     temporary_repository = create_repository_response.json()
+    logger.info(f'Bothub temporary repository created "{temporary_repository.get("name")}"')
+
     delete_repository_response = bothub.delete_repository(
         temporary_repository.get('owner__nickname'),
         temporary_repository.get('slug'))
-    delete_repository_response.raise_for_status()
+
+    try:
+        delete_repository_response.raise_for_status()
+    except Exception as e:
+        logger.warning('Bothub temporary repository not deleted, manually delete')
+        logger.debug(delete_repository_response.text)
+
+    logger.info(f'Bothub temporary repository deleted')
